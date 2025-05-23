28e715d3f0dc4430d40928a29e94e17a46fab645
Team Produzione Istrice <produzione@istricesrl.it>
2025-05-23 16:59:29 +0200

    debug e refactoring

---
+++ _src/_config/_940.session.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+
+    /**
+     * registrazione della timestamp di utilizzo della sessione
+     * ========================================================
+     * 
+     * 
+     */
+
+    // timestamp dell'ultima azione sulla sessione
+	$_SESSION['used']			= time();
+    $_SESSION['ip']				= $_SERVER['REMOTE_ADDR'];
+    $_SESSION['user_agent']		= $_SERVER['HTTP_USER_AGENT'];
---
+++ _src/_inc/_macro/_app.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * 
+     *
+     *
+     *
+     *
+     * TODO documentare
+     *
+     *
+     */
