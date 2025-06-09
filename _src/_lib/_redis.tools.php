89ff9f9577ad4389527e058e8a16d6341d0267b1
Team Produzione Istrice <produzione@istricesrl.it>
2025-06-09 11:20:21 +0200

    debug redis tools

---
+++ _src/_lib/_redis.tools.php
@@ -0,0 +1,160 @@
+<?php
+
+    /**
+     * questo file contiene funzioni per l'utilizzo di redis
+     *
+     *
+     *
+     *
+     * TODO documentare
+     *
+     */
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisUniqueKey( &$k ) {
+
+        if( strpos( $k, REDIS_UNIQUE_SEED ) === false ) {
+            $k = REDIS_UNIQUE_SEED . $k;
+        }
+
+        return $k;
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisAddKeyAgeSuffix( $k ) {
+
+        if( substr( $k, -4 ) != '_AGE' ) {
+            $k .= '_AGE';
+        }
+
+        return $k;
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisWrite( $conn, $key, $data, $ttl = REDIS_DEFAULT_TTL ) {
+
+        redisUniqueKey( $key );
+
+        if( empty( $conn ) ) {
+
+            logWrite( 'connessione al server assente per scrivere la chiave: ' . $key, 'redis' );
+
+            return false;
+
+        } else {
+
+            $r = $conn->set( $key, $data );
+            $conn->expire( $key, $ttl );
+
+            if( $r == false ) {
+                logWrite( 'impossibile scrivere la chiave: ' . $key, 'redis', LOG_ERR );
+            } else {
+                $r = $conn->set( redisAddKeyAgeSuffix( $key ), time() );
+                $conn->expire( redisAddKeyAgeSuffix( $key ), $ttl );
+                logWrite( 'scrittura effettuata, chiave: ' . redisAddKeyAgeSuffix( $key ), 'redis' );
+            }
+
+            return $r;
+
+        }
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisRead( $conn, $key ) {
+
+        redisUniqueKey( $key );
+
+        if( empty( $conn ) ) {
+
+            logWrite( 'connessione al server assente per leggere la chiave: ' . $key, 'redis' );
+
+            return false;
+
+        } else {
+
+            $r = $conn->get( $key );
+
+            if( $r == false ) {
+                logWrite( 'impossibile leggere la chiave: ' . $key, 'redis' );
+            } else {
+                logWrite( 'lettura effettuata, chiave: ' . $key, 'redis' );
+            }
+
+            return $r;
+
+        }
+
+    }
+
+    /**
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisGetKeyAge( $conn, $key ) {
+
+        return redisRead( $conn, redisAddKeyAgeSuffix( $key ) );
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisDelete( $conn, $key ) {
+
+        redisUniqueKey( $key );
+
+        if( ! empty( $conn ) ) {
+            return $conn->del( $key );
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     */
+    function redisFlush( $conn ) {
+
+	    return $conn->flushall();
+
+    }
+
+
