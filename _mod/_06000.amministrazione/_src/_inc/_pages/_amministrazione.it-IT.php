aba0223381abc3be7148feaf2ced4f059a29a779
produzione Istrice <produzione@istricesrl.it>
2025-11-04 12:00:38 +0100

    lavoro all'amministrazione

--- _mod/_06000.amministrazione/_src/_inc/_pages/_amministrazione.it-IT.php
+++ _mod/_06000.amministrazione/_src/_inc/_pages/_amministrazione.it-IT.php
@@ -74,3 +74,60 @@
 	    'etc'				=> array( 'tabs'	=> 'amministrazione.archivio' )
 	);
 
+	// dashboard amministrazione
+	$p['amministrazione.ciclo.attivo'] = array(
+	    'sitemap'		=> false,
+	    'title'			=> array( $l		=> 'amministrazione ciclo attivo' ),
+	    'h1'			=> array( $l		=> 'ciclo attivo' ),
+	    'parent'		=> array( 'id'		=> 'amministrazione' ),
+	    'template'		=> array( 'path'	=> '_src/_tpl/_athena/', 'schema' => 'amministrazione.ciclo.attivo.twig' ),
+	    'macro'			=> array( $m . '_src/_inc/_macro/_amministrazione.ciclo.attivo.php' ),
+		'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'			=> array( 'tabs'	=> array(	'amministrazione.ciclo.attivo',
+														'amministrazione.ciclo.attivo.tools'
+														 ) ),
+		'menu'				=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'ciclo attivo' ),
+																		'priority'	=> '1100' ) ) )														
+	);
+
+    // tools archivio amministrazione
+	$p['amministrazione.ciclo.attivo.tools'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-cogs" aria-hidden="true"></i>',
+	    'title'				=> array( $l		=> 'azioni amministrazione ciclo attivo' ),
+	    'h1'				=> array( $l		=> 'azioni' ),
+	    'parent'			=> array( 'id'		=> 'amministrazione.ciclo.attivo' ),
+	    'template'			=> array( 'path'	=> '_src/_tpl/_athena/', 'schema' => 'default.tools.twig' ),
+	    'macro'				=> array( $m . '_src/_inc/_macro/_amministrazione.ciclo.attivo.tools.php' ),
+	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+	    'etc'				=> array( 'tabs'	=> 'amministrazione.ciclo.attivo' )
+	);
+
+	// dashboard amministrazione
+	$p['amministrazione.ciclo.passivo'] = array(
+	    'sitemap'		=> false,
+	    'title'			=> array( $l		=> 'amministrazione ciclo passivo' ),
+	    'h1'			=> array( $l		=> 'ciclo passivo' ),
+	    'parent'		=> array( 'id'		=> 'amministrazione' ),
+	    'template'		=> array( 'path'	=> '_src/_tpl/_athena/', 'schema' => 'amministrazione.ciclo.passivo.twig' ),
+	    'macro'			=> array( $m . '_src/_inc/_macro/_amministrazione.ciclo.passivo.php' ),
+		'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'			=> array( 'tabs'	=> array(	'amministrazione.ciclo.passivo',
+														'amministrazione.ciclo.passivo.tools'
+														 ) ),
+		'menu'				=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'ciclo passivo' ),
+																		'priority'	=> '2100' ) ) )														
+	);
+
+    // tools archivio amministrazione
+	$p['amministrazione.ciclo.passivo.tools'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-cogs" aria-hidden="true"></i>',
+	    'title'				=> array( $l		=> 'azioni amministrazione ciclo passivo' ),
+	    'h1'				=> array( $l		=> 'azioni' ),
+	    'parent'			=> array( 'id'		=> 'amministrazione.ciclo.passivo' ),
+	    'template'			=> array( 'path'	=> '_src/_tpl/_athena/', 'schema' => 'default.tools.twig' ),
+	    'macro'				=> array( $m . '_src/_inc/_macro/_amministrazione.ciclo.passivo.tools.php' ),
+	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+	    'etc'				=> array( 'tabs'	=> 'amministrazione.ciclo.passivo' )
+	);
