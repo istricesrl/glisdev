3ed1b069cfe5e489cb10f8381cb6364290aa23e0
elisabetta <elisabetta.comani@gmail.com>
2025-11-09 23:53:47 +0100

    funzioni commentate

--- _src/_lib/_random.tools.php
+++ _src/_lib/_random.tools.php
@@ -1,21 +1,27 @@
 <?php
 
     /**
-     *breve descrizione
-     *introduzione
-     *=============
+     * libreria di generatori di stringhe casuali
      *
-     *funzione            |descrizione
-     * -------------------|------
-     * getToken()         |
-     * getPassword()      |
+     * introduzione
+     * =============
+     * Questa libreria fornisce strumenti utili per ottenere stringhe casuali, come token o password.
+     *
+     * funzione           | descrizione
+     * -------------------|---------------------------------------------------------------------------
+     * getToken()         | restituisce un token generato casualmente a partire dal tempo corrente 
+     * getPassword()      | restituisce una password casuale di lunghezza definita   
      *
      */
 
     /**
-     *
-     * @todo documentare
-     *
+     * restituisce un token generato casualmente a partire dal tempo corrente 
+     * 
+     * Questa funzione moltiplica il timestamp in millisecondi per un numero casuale e restituisce il risultato
+     * crittografato con MD5.
+     * 
+     * @return   string    hash MD5 (32 caratteri esadecimali)
+     * 
      */
     function getToken() {
 
@@ -24,16 +30,26 @@
     }
 
     /**
-     *
-     * @todo documentare
-     *
+     * restituisce una password casuale di lunghezza definita 
+     * 
+     * Questa funzione può essere utilizzata per generare una password casuale di lunghezza definita e contenente
+     * i caratteri forniti.
+     * 
+     *  @param   int       $length      la lunghezza della password da generare
+     *  @param   string    $keyspace    contiene i caratteri da utilizzare per comporre la password
+     * 
+     *  @return  string                 la password generata
+     * 
      */
     function getPassword( $length = 16, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_!?%' ) {
 
+        // inizializza una stringa vuota che sarà popolata con i caratteri casuali scelti
         $str = '';
 
+        // $max è la lunghezza in byte di $keyspace
         $max = mb_strlen( $keyspace, '8bit' ) - 1;
 
+        // la stringa di lunghezza $lenght viene composta aggiungendo ad ogni ciclo un carattere selezionato casualmente da $keyspace
         for( $i = 0; $i < $length; ++$i ) {
             $str .= $keyspace[ random_int( 0, $max ) ];
         }
