131eda3635d7b64031ec90eed57df6745440ded1
Elisabetta Comani <elisabetta.comani@gmail.com>
2025-11-28 17:07:25 +0100

    preparazione scheda video articolo

---
+++ _mod/_VI000.video/_src/_inc/_macro/_contenuti.archivio.video.form.php
@@ -0,0 +1,22 @@
+<?php
+
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     * 
+     */
+
+    // tabella gestita
+    $ct['form']['table'] = 'video';
+
+
+ 
+
+    // macro di default
+    require DIR_SRC_INC_MACRO . '_default/_default.form.php';
+
