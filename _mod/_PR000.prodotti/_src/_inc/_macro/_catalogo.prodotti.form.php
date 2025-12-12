d8e417816d825c2acfe13d2e0303175fbbd99638
elisabetta <elisabetta.comani@gmail.com>
2025-12-12 15:00:39 +0100

    macro e template catalogo.prodotti.form

--- _mod/_PR000.prodotti/_src/_inc/_macro/_catalogo.prodotti.form.php
+++ _mod/_PR000.prodotti/_src/_inc/_macro/_catalogo.prodotti.form.php
@@ -20,5 +20,31 @@
         'SELECT id, __label__ FROM tipologie_prodotti_view ORDER BY __label__ ASC'
     );
 
+    /**
+     * dati delle tendine
+     * ==================
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+    // tendina produttori
+    $ct['etc']['select']['produttori'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM anagrafica_view_static WHERE se_produttore = 1'
+    );
+
+    // tendina marchi
+    $ct['etc']['select']['marchi'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM marchi_view'
+    );
+
     // macro di default
     require DIR_SRC_INC_MACRO . '_default/_default.form.php';
