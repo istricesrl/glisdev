61bc942f37d4bc00009ba02ceabbc61975e50844
produzione Istrice <produzione@istricesrl.it>
2025-12-27 14:12:04 +0100

    lavoro a macro e metadati

--- _mod/_CO000.contenuti/_src/_inc/_macro/_contenuti.pagine.form.metadati.php
+++ _mod/_CO000.contenuti/_src/_inc/_macro/_contenuti.pagine.form.metadati.php
@@ -15,6 +15,9 @@
     // tabella gestita
     $ct['form']['table'] = 'pagine';
 
+    // tendina lingue
+    $ct['etc']['select']['lingue'] = $cf['localization']['languages'];
+
     // macro di default
     require DIR_SRC_INC_MACRO . '_default/_default.form.php';
 
