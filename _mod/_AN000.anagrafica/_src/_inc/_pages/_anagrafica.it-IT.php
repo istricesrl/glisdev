89c7cbbe81ebbf7c85fc668ce3a6e478d0a797fe
produzione Istrice <produzione@istricesrl.it>
2026-01-22 10:53:19 +0100

    scheda stampe in categorie anagrafica form

--- _mod/_AN000.anagrafica/_src/_inc/_pages/_anagrafica.it-IT.php
+++ _mod/_AN000.anagrafica/_src/_inc/_pages/_anagrafica.it-IT.php
@@ -500,7 +500,21 @@
         'macro'                => array( $m . '_src/_inc/_macro/_categorie.anagrafica.form.php' ),
         'auth'                => array( 'groups'    => array(    'roots', 'staff' ) ),
         'etc'                => array( 'tabs'    => array(    'categorie.anagrafica.form',
+                                                            'categorie.anagrafica.form.stampe',
                                                             'categorie.anagrafica.form.tools' ) )
+    );
+
+    // categorie anagrafica form stampe
+    $p['categorie.anagrafica.form.stampe'] = array(
+        'sitemap'            => false,
+        'icon'                => '<i class="fa fa-print" aria-hidden="true"></i>',
+        'title'                => array( $l        => 'categorie anagrafica form stampe' ),
+        'h1'                => array( $l        => 'stampe' ),
+        'parent'            => array( 'id'        => 'categorie.anagrafica.view' ),
+        'template'            => array( 'path'    => '_src/_tpl/_athena/', 'schema' => 'default.tools.twig' ),
+        'macro'                => array( $m . '_src/_inc/_macro/_categorie.anagrafica.form.stampe.php' ),
+        'auth'                => array( 'groups'    => array(    'roots', 'staff' ) ),
+        'etc'                => array( 'tabs'    => 'categorie.anagrafica.form' )
     );
 
     // tools account
