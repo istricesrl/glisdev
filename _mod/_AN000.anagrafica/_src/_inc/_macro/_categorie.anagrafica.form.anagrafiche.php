45ab5acc71ebe772659e72920c44963e48d0cfa4
produzione Istrice <produzione@istricesrl.it>
2026-04-21 19:19:15 +0200

    wip

--- _mod/_AN000.anagrafica/_src/_inc/_macro/_categorie.anagrafica.form.anagrafiche.php
+++ _mod/_AN000.anagrafica/_src/_inc/_macro/_categorie.anagrafica.form.anagrafiche.php
@@ -22,6 +22,55 @@
     );
 
     /**
+     * gestione dati form associazione rapida
+     * ======================================
+     * 
+     * 
+     * 
+     */
+
+    // gestione link anagrafica categorie_anagrafica
+    if( isset( $_REQUEST['__link_anagrafica__']['id_anagrafica'] ) ) {
+
+        // link anagrafica
+        // mysqlQuery(
+        //     $cf['mysql']['connection'],
+        //     'UPDATE anagrafica SET id_ranking = ? WHERE id = ?',
+        //     array(
+        //         array( 's' => $_REQUEST[ $ct['form']['table'] ]['id'] ),
+        //         array( 's' => $_REQUEST['__link_anagrafica__']['id_anagrafica'] )
+        //     )
+        // );
+
+        mysqlInsertRow(
+                                $cf['mysql']['connection'],
+                                array(
+                                    'id_anagrafica' => $_REQUEST['__link_anagrafica__']['id_anagrafica'],
+                                    'id_categoria' => $_REQUEST[ $ct['form']['table'] ]['id'],
+                                    'timestamp_inserimento' => time(),
+                                ),
+                                'anagrafica_categorie'
+                            );
+
+        //updateAnagraficaViewStatic( $_REQUEST['__link_anagrafica__']['id_anagrafica'] );
+
+    } elseif( isset( $_REQUEST['__scollega_anagrafica__'] ) ) {
+
+        // scollega anagrafica
+        mysqlQuery(
+            $cf['mysql']['connection'],
+            'DELETE FROM anagrafica_categorie WHERE id = ?',
+            array(
+                array( 's' => $_REQUEST['__scollega_anagrafica__'] )
+            )
+        );
+
+        //updateAnagraficaViewStatic( $_REQUEST['__scollega_anagrafica__'] );
+
+    }
+
+
+    /**
      * dati della view
      * ===============
      * 
@@ -62,6 +111,13 @@
         ),
     );
 
+        // inserimento rapido
+    $ct['etc']['include']['insert'][] = array(
+        'name' => 'insert',
+        'file' => 'inc/categorie.anagrafica.form.anagrafiche.link.twig',
+        'fa' => 'fa-link'
+    );
+
     /**
      * macro di default
      * ================
@@ -76,6 +132,19 @@
 
     // macro di default
     require DIR_SRC_INC_MACRO . '_default/_default.form.php';
+
+    // elaborazione dati
+    if( !empty( $ct['view']['data'] ) ){
+		foreach ( $ct['view']['data'] as &$row ) {
+
+            $buttons = '';
+
+            $buttons .=  '<a href="?categorie_anagrafica[id]='.$_REQUEST[ $ct['form']['table'] ]['id'].'&__scollega_anagrafica__='.$row['id'].'"><span class="media-left"><i class="fa fa-chain-broken"></i></span></a>';
+
+            $row[ NULL ] = $buttons;
+
+        }
+	}
 
     /**
      * debug del form
