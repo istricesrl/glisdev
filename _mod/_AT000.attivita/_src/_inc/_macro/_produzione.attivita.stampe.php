2a2f9df1fc9c09a1eee8c941ef338d9a05460618
produzione Istrice <produzione@istricesrl.it>
2026-05-08 16:41:29 +0200

    aggiunta scheda attivita stampe

---
+++ _mod/_AT000.attivita/_src/_inc/_macro/_produzione.attivita.stampe.php
@@ -0,0 +1,22 @@
+<?php
+
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * TODO documentare
+     * 
+     * 
+     */
+
+    // tabella gestita
+   
+
+    // gruppi di controlli
+    $ct['page']['contents']['metros'] = array(
+        '31.pdf' => array(
+            'label' => 'stampe PDF'
+        )
+    );
