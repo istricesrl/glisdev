bd93488b2b218fa91249238d5d6e70aa896e9967
produzione Istrice <produzione@istricesrl.it>
2026-05-13 18:41:06 +0200

    aggiunta tendine

--- _mod/_VI000.video/_src/_inc/_macro/_contenuti.archivio.video.form.collegamenti.php
+++ _mod/_VI000.video/_src/_inc/_macro/_contenuti.archivio.video.form.collegamenti.php
@@ -38,6 +38,38 @@
         'SELECT id, __label__ FROM categorie_notizie_view ORDER BY __label__'
     );
 
+    //tendina prodotti
+    $ct['etc']['select']['prodotti'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM prodotti_view ORDER BY __label__'
+    );
+
+    //tendina categorie prodotti
+    $ct['etc']['select']['categorie_prodotti'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM categorie_prodotti_view ORDER BY __label__'
+    );
+
+    //tendina prodotti
+    $ct['etc']['select']['marchi'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM marchi_view ORDER BY __label__'
+    );
+
+    //tendina prodotti
+    $ct['etc']['select']['articoli'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM articoli_view ORDER BY __label__'
+    );
+
     // macro di default
     require DIR_SRC_INC_MACRO . '_default/_default.form.php';
 
